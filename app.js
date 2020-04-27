c738afb10376b11123be8ac67db7f62020389107
diptesh6501 <diptesh1993@gmail.com>
2020-04-27 22:37:00 +0530

    returns parsed information with promise

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 const multer = require('multer');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 8080
 const pdf = require('pdf-parse');
@@ -16,10 +17,11 @@ let availableSkills = ['angular', 'react', 'java', 'qa', 'node',
 let toolsTextPattern = ['jenkins', 'ci/cd', 'maven', 'circleci', 'aws', 'selenium'];
 let ideTextPattern = ['webstorm', 'code', 'visual studio', 'eclipse', 'sublime text'];
 let phoneNoTextPattern = ['phone', 'phno', 'Mob', 'Mobile', 'number']
+let fileParsedPath;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
 
@@ -34,74 +36,91 @@ app.use(function (req, res, next) {
 var storage = multer.diskStorage(
     {
         destination: './uploads/',
-        filename: function ( req, file, cb ) {
-            cb( null, file.originalname+".pdf");
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
         }
     }
 );
 
-var upload = multer( { storage: storage } );
+var upload = multer({ storage: storage });
 
 
-app.post('/uploadFile', upload.single('file'), function(req, res) {
- console.log(req.file);
- console.log('original Name', req.file.originalname);
-res.send("file saved on server");
+app.post('/uploadFile', upload.single('file'), async (req, res) => {
+    console.log(req.file);
+    console.log('original Name', req.file.originalname);
+    fileParsedPath = './uploads/' + req.file.originalname;
+    parseResume(fileParsedPath).then((data) => {
+        res.json({
+            'parsedResumeInformation': JSON.stringify(data)
+        });
+    }).catch((error) => {
+        console.log('errror', error);
+    })
+
 });
+function parseResume(path) {
+    return new Promise((resolve, reject) => {
+
+        if (path) {
+            const resume = new ResumeParser(path);
+            resume.parseToJSON()
+                .then(async data => {
+                    parsedDocument = data.parts;
+                    console.log('parsed document', parsedDocument);
+
+                    // extracting  name 
+                    if (parsedDocument.name) {
+                        parsedInfo.name = parsedDocument.name;
+                    }
+                    //extracting email 
+                    if (parsedDocument.email) {
+                        parsedInfo.email = parsedDocument.email;
+                    }
+                    // extracting skills from technology section 
+                    if (Boolean(data.parts.hasOwnProperty('technology'))) {
+                        let splittedTechnology = parsedDocument.technology.split(',');
+                        parsedInfo.skills = [...new Set(await extractInfo(availableSkills, splittedTechnology))];
+                        parsedInfo.tools = [...new Set(await extractInfo(toolsTextPattern, splittedTechnology))];
+                        parsedInfo.ide = [...new Set(await extractInfo(ideTextPattern, splittedTechnology))];
+
+                    }
+                    // extracting skills from skills
+                    if (Boolean(data.parts.hasOwnProperty('skills'))) {
+                        let splittedSkills = parsedDocument.skills.split(',');
+                        parsedInfo.skills = [...new Set(await extractInfo(availableSkills, splittedSkills))];
+                        parsedInfo.tools = [...new Set(await extractInfo(toolsTextPattern, splittedSkills))];
+                        parsedInfo.ide = [...new Set(await extractInfo(ideTextPattern, splittedSkills))];
 
 
-const resume = new ResumeParser("./functionalSample.pdf");
-resume.parseToJSON()
-    .then(async data => {
-        parsedDocument = data.parts;
-        console.log('parsed document', parsedDocument);
-        
-        // extracting  name 
-        if (parsedDocument.name) {
-            parsedInfo.name = parsedDocument.name;
-        }
-        //extracting email 
-        if (parsedDocument.email) {
-            parsedInfo.email = parsedDocument.email;
-        }
-        // extracting skills from technology section 
-        if (Boolean(data.parts.hasOwnProperty('technology'))) {
-            let splittedTechnology = parsedDocument.technology.split(',');
-            parsedInfo.skills = [...new Set(await extractInfo(availableSkills, splittedTechnology))];
-            parsedInfo.tools = [...new Set(await extractInfo(toolsTextPattern, splittedTechnology))];
-            parsedInfo.ide = [...new Set(await extractInfo(ideTextPattern, splittedTechnology))];
+                    }
+                    // extracting phone number
+                    if (Boolean(data.parts.hasOwnProperty('summary'))) {
+                        let splittedSummary = parsedDocument.summary.split(',');
+                        parsedInfo.phoneNumber = await extractPhoneNumber(phoneNoTextPattern, splittedSummary);
+                    }
+                    console.log('parsed info', parsedInfo);
+                    resolve(parsedInfo)
 
-        }
-        // extracting skills from skills
-        if (Boolean(data.parts.hasOwnProperty('skills'))) {
-            let splittedSkills = parsedDocument.skills.split(',');
-            parsedInfo.skills = [...new Set(await extractInfo(availableSkills, splittedSkills))];
-            parsedInfo.tools = [...new Set(await extractInfo(toolsTextPattern, splittedSkills))];
-            parsedInfo.ide = [...new Set(await extractInfo(ideTextPattern, splittedSkills))];
+                })
+                .catch(error => {
+                    console.error(error);
+                    reject('file path is required');
 
-
-        }
-        // extracting phone number
-        if (Boolean(data.parts.hasOwnProperty('summary'))) {
-            let splittedSummary = parsedDocument.summary.split(',');
-            parsedInfo.phoneNumber = await extractPhoneNumber(phoneNoTextPattern, splittedSummary);
+                });
         }
     })
-    .catch(error => {
-        console.error(error);
-    });
+}
+
 
 
 
 app.get('/', (req, res) => {
-   res.send({
-       'message': 'welcome to recruit boot ai'
-   })
+    res.send({
+        'message': 'welcome to recruit boot ai'
+    })
 })
 
-setTimeout(() => {
-    console.log('parsedInfo', parsedInfo);
-}, 3000)
+
 
 
 app.listen(port, (err, res) => {
